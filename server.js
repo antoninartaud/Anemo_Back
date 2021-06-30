4d6a592da13fe64f633ecfb59dece9c247fff1f4
Mugi <g.mugi93140@gmail.com>
2021-06-30 17:02:56 +0200

    connexion mongodb

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const mongoose = require ("mongoose")
+
+
+const {port, mongoURL}= require("./config")
+
+mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+  if (err) {
+      console.error(err);
+  } else {
+      console.log("I'm connected to the database")
+  }
+})
+
 
 const app = express();
-
-const port = 8000;
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.json({
